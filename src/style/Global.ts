3925174a444324e17eb5b4f1c360a1823fbb830d
ankurqss2009 <ankur@qsstechnosoft.com>
2022-02-10 23:04:19 +0530

    ni

---
+++ src/style/Global.ts
@@ -0,0 +1,187 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    //background-color: ${({ theme }) => 'red' || theme.colors.background};
+    //color: blue!important;
+    //radial-gradient(50% 50% at 50% 50%, #fc077d10 0%, rgba(255, 255, 255, 0) 100%);
+    background-image: url("images/background.jpeg");
+    background-repeat: no-repeat;
+    
+    img {
+      height: auto;
+      max-width: 100%;
+      
+    }
+    .fA-DbKA{
+      display: none!important;
+      background: gold;
+    }
+    nav.sc-bTvRPi.ckbSWR {
+      background: #8BD4FF;
+    }
+    .sc-dQppl{
+      background: #8BD4FF;
+    }
+    .sc-dwfUOf.kdRZDD {
+      background: #8BD4FF;
+    }
+    .sc-dQppl.bjeVKm.sc-bGqQkm.KzZQA {
+      background: #8BD4FF;
+    }
+    .sc-dkAroR.imUppv {
+      background: #1E9DE9;
+    }
+    button.sc-dlfnbm.kkIacT {
+      background: #1E9DE9;
+    }
+    .sc-fbkhIv.fA-DbKA {
+      display: none;
+    }
+    .ipZpUs {
+      color: #ffffff;}
+    .bdDvuW svg {
+      fill: #ffffff;
+    }
+    .sc-dwfUOf.ebPAtq {
+      background: #8bd4ff;
+    }
+    button.sc-dlfnbm.kyLmik {
+      background: #1e9de9;
+      border: 1px solid #fff;
+    }
+    .bdDvuW:hover {
+      background-color: #1e9de9;
+    }
+    .lftvyW svg {
+      fill: #ffffff;
+    }
+    .lftvyW:hover {
+      background-color: #1e9de9;
+    }
+    .sc-nFpLZ.dKtzmE {
+      color: #1e9de9;
+    }
+    .sc-nFpLZ.ledHps {
+      color: #1e9de9;
+    }
+    .qtTdY {
+      fill: #1e9de9;}
+    h2.sc-gsTCUz.sc-hHftDr.gAZIAq.ddLuh {
+      color: #fff;
+    }
+
+    .sc-gsTCUz.ZFEBg {
+      color: #fff;
+    }
+    div#pair {
+      color: #f9f9f9;
+    }
+    button.sc-dlfnbm.iLxwUA.sc-hKgILt.hElrcP {
+      color: #1e9de9;
+    }
+    .kyLmik{
+      background-color: #1e9de9;
+      border: 2px solid;
+    }
+    .ftKFin {
+      background-color: transparent;
+      color: #1e9de9;
+    }
+    input.sc-tkKAw.kDPwqj.token-amount-input::placeholder{
+      color: #fff;
+    }
+    .ejGDLu {
+      color: #1e9de9;}
+    button.sc-dlfnbm.dzRAJz {
+      color: #1E9DE9;
+    }
+    h2.sc-gsTCUz.sc-hHftDr.cRGyYT.ddLuh {
+      color: #1E9DE9;
+    }
+    .ifMokV {
+      color: #1e9de9;}
+    .hSfeuA {
+      color: #1e9de9;
+      background-color: #eff4f5;}
+    .gkZZw {
+      background-color: #eff4f5;
+      color: #1e9de9;}
+    .sc-gkdzZj.jmnchU {
+      color: #1e9de9;
+    }
+    .sc-gyUeRy.cXopsW {
+      color: #1e9de9;
+    }
+    .hqcljE {
+      color: #1e9de9;}
+
+    .icYoWg {
+      background-color: #8bd4ff;
+      color: white;
+    }
+    .gAZIAq {
+      color: #1e9de9;
+    }
+    .gelZTi {
+      background-color: #eff4f5;
+      color: #1e9de9;
+    }
+    .gelZTi:focus:not(:disabled) {
+      box-shadow: 0px 0px 0px 1px #8bd4ff, 0px 0px 0px 4px #8bd4ff;
+    }
+    .hSfeuA:focus:not(:disabled) {
+      box-shadow: 0px 0px 0px 1px #8bd4ff, 0px 0px 0px 4px #8bd4ff;
+    }
+    button.sc-dlfnbm.bwTqvv {
+      color: #ffffff;
+      background: #8bd4ff;
+      background: #1e9de9;
+      border: 2px solid;
+    }
+    .cRGyYT {
+      color: #1e9de9;}
+    .hUWxlM {
+      fill: #1e9de9;}
+    .qtTdY {
+      fill: #ffffff;
+    }
+    .lftvyW {
+      box-shadow: inset 4px 0px 0px #ffffff;
+    }
+    .dIMdUw {
+      color: #ffffff;
+    }
+    .dKtzmE {
+
+      box-shadow: inset 4px 0px 0px #ffffff;
+    }
+    .ebFFe {
+      color: #1e9de9;}
+    .dzeiMN {background-color: #1e9de9;
+      border: 2px solid;
+      border-color: #ffffff;
+      box-shadow: none;
+      color: #ffffff;
+    }
+    .ligcQc {
+      color: #1e9de9;}
+    .ccVFpt {
+
+      color: #1e9de9;
+    }
+    .gDMfPh {
+      fill: #1e9de9;}
+    button.sc-dlfnbm.iLxwUA.sc-hKgILt.hElrcP svg.sc-bdfBwQ.qtTdY {
+      fill: #1e9de9;
+    }
+    nav.sc-bTvRPi.dhgjZW {
+      background-color: #8bd4ff !important;
+    }
+    .sc-dwfUOf.gpmkJL {
+      background-color: #8BD4FF;
+    }
+  }
+`
+
+export default GlobalStyle
